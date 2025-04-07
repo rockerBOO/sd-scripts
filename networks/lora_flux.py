cf46824fe1be3379e693bd12dbce0ce4615632f4
rockerBOO <rockerboo@gmail.com>
2025-04-07 14:17:41 -0400

    Fix _org_lora_up

--- networks/lora_flux.py
+++ networks/lora_flux.py
@@ -93,6 +93,16 @@ class LoRAModule(torch.nn.Module):
         self.sum_grads = None
         self.sum_squared_grads = None
 
+        self.ggpo_sigma = ggpo_sigma
+        self.ggpo_beta = ggpo_beta
+
+        if self.ggpo_beta is not None and self.ggpo_sigma is not None:
+            self.combined_weight_norms = None
+            self.grad_norms = None
+            self.perturbation_norm_factor = 1.0 / math.sqrt(org_module.weight.shape[0])
+            self.initialize_norm_cache(org_module.weight)
+            self.org_module_shape: tuple[int] = org_module.weight.shape
+
 
     def initialize_weights(self, org_module: torch.nn.Module, initialize: Optional[str], device: Optional[torch.device]):
         """
@@ -131,11 +141,12 @@ class LoRAModule(torch.nn.Module):
                 else:
                     initialize_lora(lora_down, lora_up)
 
-        if self._org_lora_up is not None and self._org_lora_down is not None:
-            # TODO: Capture option if we should keep on VRAM
-            # offloading to CPU
-            self._org_lora_up = self._org_lora_up.to("cpu")
-            self._org_lora_down = self._org_lora_down.to("cpu")
+        if hasattr(self, "_org_lora_up") and hasattr(self, "_org_lora_down"):
+            if self._org_lora_up is not None and self._org_lora_down is not None:
+                # TODO: Capture option if we should keep on VRAM
+                # offloading to CPU
+                self._org_lora_up = self._org_lora_up.to("cpu")
+                self._org_lora_down = self._org_lora_down.to("cpu")
 
     def apply_to(self):
         self.org_forward = self.org_module.forward
